a4f5119235746b7b756b30d30a6afb99cf10128e
m-cyx <markpixel05@gmail.com>
2022-05-25 22:46:32 +0300

    creating model and applying migrations

--- learnsite/news/models.py
+++ learnsite/news/models.py
@@ -1,3 +1,16 @@
+from distutils.command.upload import upload
+from email import contentmanager
+from email.mime import image
+from pyexpat import model
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='photos/%Y/%m/d%/')
+    is_published = models.BooleanField(default=True)
