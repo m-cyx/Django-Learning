43b2d55d581da7a7d0bd382d0c85a08033b7115b
m-cyx <markpixel05@gmail.com>
2022-05-26 16:38:26 +0300

    Creating admin panel

--- learnsite/news/models.py
+++ learnsite/news/models.py
@@ -2,6 +2,7 @@ from distutils.command.upload import upload
 from email import contentmanager
 from email.mime import image
 from pyexpat import model
+from tabnanny import verbose
 from turtle import title
 from django.db import models
 
@@ -9,12 +10,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='photos/%Y/%m/d%/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Cодержание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+    image = models.ImageField(upload_to='photos/%Y/%m/d%/', blank='True')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
