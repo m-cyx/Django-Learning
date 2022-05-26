033333727e8c61459e9bddbbcacdb9441e2688c4
m-cyx <markpixel05@gmail.com>
2022-05-26 17:20:10 +0300

    add category as ForeignKey and middind admin panel

--- learnsite/news/admin.py
+++ learnsite/news/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+# Тут регистрируем модели для админки
 from .models import News
+from .models import Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'created_at',
+                    'updated_at', 'is_published', 'category') # Какие поля оттображать в списке 
+    list_display_links = ('id', 'title') # Какие поля являются ссылкой
+    search_fields = ('title', 'content', 'category') # По каким полям можно искать
+    list_editable = ('is_published',) # Какие поля можно редактировать из админики 
+    list_filter = ('is_published', 'category') # По каким полям фильтруем
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
     list_display_links = ('id', 'title')
-    search_fields = ('title', 'content')
+    search_fields = ('title',)
 
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- learnsite/news/models.py
+++ learnsite/news/models.py
@@ -4,6 +4,7 @@ from email.mime import image
 from pyexpat import model
 from tabnanny import verbose
 from turtle import title
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
@@ -12,10 +13,15 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Заголовок')
     content = models.TextField(blank=True, verbose_name='Cодержание')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name='Дата редактирования')
     image = models.ImageField(upload_to='photos/%Y/%m/d%/', blank='True')
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    is_published = models.BooleanField(
+        default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey(
+        'Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -24,3 +30,16 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=150, db_index=True, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
