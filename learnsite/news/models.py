f3b32bbcb65a62cd84ec1d246eb42a735a7b8f1e
m-cyx <markpixel05@gmail.com>
2022-05-26 13:19:36 +0300

    creating template

--- learnsite/news/models.py
+++ learnsite/news/models.py
@@ -7,6 +7,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class News(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
@@ -14,3 +15,6 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='photos/%Y/%m/d%/')
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
--- learnsite/news/views.py
+++ learnsite/news/views.py
@@ -1,6 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
+
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello World')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Cписок новостей'
+    }
+    return(render(request, 'news/index.html', context))
